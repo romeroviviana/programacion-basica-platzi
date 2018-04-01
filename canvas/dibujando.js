faf421578937b26864f62f65503e0fda3c621df6
vivianaprr <romerovivianapr@gmail.com>
2018-04-01 08:41:30 -0400

    ciclo for while y do while

--- canvas/dibujando.js
+++ canvas/dibujando.js
@@ -1,7 +1,32 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var l = 0;
+var lineas = 30;
+var a, b;
+var colorLinea1 = "purple";
+var colorLinea2 = "gray";
 
-dibujarLinea("purple", 0, 0, 300, 300 );
+do{
+	a = 10 * l;
+	b = 10 * (l+1);
+	dibujarLinea(colorLinea1, 0, a, b, 300 );
+	l++;
+}
+while(l < lineas);
+dibujarLinea(colorLinea1, 1, 1, 1, 299);
+dibujarLinea(colorLinea1, 1, 299, 299, 299);
+
+
+
+for(l=0; l<lineas; l++){
+	a = 10 * l;
+	b = 300 - (a+10);
+	dibujarLinea(colorLinea2, 300, a, b, 300 );
+}
+dibujarLinea(colorLinea2, 299, 1, 299, 299);
+
+
+
 
 function dibujarLinea(color, xinicial, xfinal, yinicial, yfinal){
 	lienzo.beginPath();
