5c818b4f2d9203688211082e588d165a0670d60e
vivianaprr <romerovivianapr@gmail.com>
2018-04-01 07:40:33 -0400

    funciones en js

--- canvas/dibujando.js
+++ canvas/dibujando.js
@@ -1,9 +1,13 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath();
+dibujarLinea("purple", 0, 0, 300, 300 );
+
+function dibujarLinea(color, xinicial, xfinal, yinicial, yfinal){
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial, xfinal);
+	lienzo.lineTo(yinicial, yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
