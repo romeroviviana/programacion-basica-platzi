c6ba8317fd9e9e05a9d17a0632f9caf319d3a4e2
vivianaprr <romerovivianapr@gmail.com>
2018-04-02 08:08:11 -0400

    aprendiendo pasar valores por input de texto y botones

--- canvas/dibujando.js
+++ canvas/dibujando.js
@@ -1,37 +1,10 @@
 var d = document.getElementById("dibujito");
+var texto = document.getElementById("texto_linea");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+ancho = d.width;
+
 var lienzo = d.getContext("2d");
-var l = 0;
-var lineas = 30;
-var a, b;
-var colorLinea1 = "purple";
-var colorLinea2 = "gray";
-
-do{
-	a = 10 * l;
-	b = 10 * (l+1);
-	dibujarLinea(colorLinea1, 0, a, b, 300 );
-	l++;
-}
-while(l < lineas);
-dibujarLinea(colorLinea1, 1, 1, 1, 299);
-dibujarLinea(colorLinea1, 1, 299, 299, 299);
-
-
-
-for(l=0; l<lineas; l++){
-	a = 10 * l;
-	b = 300 - (a+10);
-	dibujarLinea(colorLinea2, 300, a, b, 300 );
-}
-dibujarLinea(colorLinea2, 299, 1, 299, 299);
-
-l = 0;
-while(l < lineas){
-	a = 10 * l;
-	b = 10 * (l+1);
-	dibujarLinea(colorLinea1, a, 0, 300, b);
-	l++;
-}
 
 function dibujarLinea(color, xinicial, xfinal, yinicial, yfinal){
 	lienzo.beginPath();
@@ -40,4 +13,20 @@ function dibujarLinea(color, xinicial, xfinal, yinicial, yfinal){
 	lienzo.lineTo(yinicial, yfinal);
 	lienzo.stroke();
 	lienzo.closePath();
+}
+
+function dibujoPorClick(){
+	var l = 0;
+	lineas = parseInt(texto.value);
+	espacio = ancho / lineas;
+	var a, b;
+	var colorLinea1 = "purple";
+
+	for(l=0; l < lineas; l++){
+		a = espacio * l;
+		b = espacio * (l+1);
+		dibujarLinea(colorLinea1, 0, a, b, ancho );	
+	}
+	dibujarLinea(colorLinea1, 1, 1, 1, ancho-1);
+	dibujarLinea(colorLinea1, 1, ancho-1, ancho-1, ancho-1);
 }
