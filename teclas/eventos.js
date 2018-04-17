9b28f463cd395138d4dd16ca577d2882a63d378f
Viviana Romero <romerovivianapr@gmail.com>
2018-04-17 08:50:03 -0400

    dibujando con teclas funciona

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,25 +1,47 @@
-document.addEventListener("keyup", dibujarConTeclas);
-
-teclas = {
+var teclas = {
 	LEFT: 37,
 	UP: 38,
 	RIGHT: 39,
 	DOWN: 40
 };
 
-function dibujarConTeclas(evento) {	
+
+document.addEventListener("keyup", dibujarTeclado);
+var cuadrito = document.getElementById("are_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+dibujarLinea("red", x - 1, y - 1, x + 1, y + 1, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo){
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.lineWidth = 3;
+	lienzo.moveTo(xinicial, yinicial);
+	lienzo.lineTo(xfinal, yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
+
+function dibujarTeclado(evento) {	
+	var colorcito = "purple";
+	var ancho = 10;
 	switch(evento.keyCode){
 		case teclas.LEFT:
-			console.log("izquierda");
+			dibujarLinea(colorcito, x, y, x - ancho, y, papel);
+			x = x - ancho;
 		break;
 		case teclas.UP:
-			console.log("arriba");
+			dibujarLinea(colorcito, x, y, x, y - ancho, papel);
+			y = y - ancho;
 		break;
 		case teclas.RIGHT:
-			console.log("derecha");
+			dibujarLinea(colorcito, x, y, x + ancho, y, papel);
+			x = x + ancho;
 		break;
 		case teclas.DOWN:
-			console.log("abajo");
+			dibujarLinea(colorcito, x, y, x, y + ancho, papel);
+			y= y + ancho;
 		break;		
 		default:
 			console.log("what ever");
