2f52dbcc960049bdbcbf4f543c78fb99625a0bc3
Viviana Romero <romerovivianapr@gmail.com>
2018-05-01 09:02:58 -0400

    villa platzi carga de tile y animales

--- villaplatzi/platzi.js
+++ villaplatzi/platzi.js
@@ -1,1 +1,39 @@
-alert("yaiii");
+var vp = document.getElementById('villaplatzi');
+var papel = vp.getContext("2d");
+var mapa = "tile.png";
+
+var fondo = new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", dibujar);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarCerdo);
+
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarVaca);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load", dibujarPollo);
+
+function dibujar() {
+	papel.drawImage(fondo, 0, 0);
+}
+function dibujarCerdo() {
+	papel.drawImage(cerdo, 0, 100);
+}
+function dibujarVaca() {
+	papel.drawImage(vaca, 250, 250);
+}
+function dibujarPollo() {
+	papel.drawImage(pollo, 300, 300);
+}
+
+
+function aleatorio(min, max) {
+	var resultado;
+	resultado = Math.floor(Math.random() * (max - min + 1) + min);
+	return resultado;
+}
