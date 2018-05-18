aed1c7cf4a1a0e80004b39ef07ff73c88f33c2e7
Viviana Romero <romerovivianapr@gmail.com>
2018-05-18 08:38:32 -0400

    terminacion de pakiman

---
+++ pakiman/pakiman.js
@@ -0,0 +1,20 @@
+class Pakiman
+{
+	constructor(n, v, a)
+	{
+		this.nombre = n;
+		this.vida = v;
+		this.ataque = a;
+		this.imagen = new Image();
+		this.imagen.src = imagenes[this.nombre];
+	}
+	hablar(){
+		alert(this.nombre);
+	}
+	mostrar(){
+		document.body.appendChild(this.imagen);
+		document.write("<br>" + this.nombre + "<br>");
+		document.write("Vida: " + this.vida + "<br>");
+		document.write("Ataque: " + this.ataque + "<hr>");
+	}
+}
